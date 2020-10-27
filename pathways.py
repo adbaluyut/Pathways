fc2fe80509990930b4aec3e9ecbf012b6b47da26
Andrew Baluyut <andrew.baluyut@gmail.com>
2020-10-26 17:35:46 -0800

    computer picks a move and returns it

--- pathways.py
+++ pathways.py
@@ -1,6 +1,9 @@
 
 from Node import Node
 import copy
+import random
+from datetime import date, datetime
+
 
 N = 5
 # N = int(input("Enter the size of the game board:\n"))
@@ -8,30 +11,59 @@ N = 5
 
 def main():
 
+    # our board is initially on a maximizing layer
     board = Node(N=N)
     board.resetGrid()
-    board.insertMove(2, 0, 'H')
-    board.insertMove(2, 2, 'H')
-    board.insertMove(3, 3, 'H')
-    board.insertMove(4, 4, 'H')
-    board.insertMove(0, 4, 'H')
-    # board.draw()
-    # getHumanPlayerMove(board)
-    # board.draw()
-    # board.generateStates()
+
+    playGame(board)
+
+
+def playGame(board):
+    
     board.draw()
-    if (board.checkForAWin('H')):
-        print('Win!')
-    else:
-        print('nah brah')
+
+    depth = 3
+    currentPlayer, maximizingPlayer = getWhoMovesFirst()
+
+    while not board.checkForAWin(currentPlayer):
+
+        if currentPlayer == 'H':
+
+            getHumanPlayerMove(board)
+            currentPlayer = 'M'
+        
+        else:
+
+            print("Computer making it's move")
+            board = minimax(board, depth, -1000, +1000, maximizingPlayer)[0]
+
+            print("printing the returned board")
+            board.draw()
+            print("________________________")
+            while board.parent.parent:
+                # print(board.parent)
+                board = board.parent
+            board.parent = None
+
+            currentPlayer = 'H'
+    
+        board.draw()
+        # break
 
 
 def getWhoMovesFirst():
 
-    return input("Who moves first? (h/m)")
+    maximizing = True
+    first = input("Who moves first? (h/m)").upper()
+
+    if first == 'H':
+        maximizing = False
+    
+    return first, maximizing
 
 
 def getHumanPlayerMove(board):
+
     invalid_move = True
     while (invalid_move):
         x = int(input('\nEnter the x coordinate of your desired move:'))
@@ -43,14 +75,54 @@ def getHumanPlayerMove(board):
         else:
             print('\nInvalid move, please try new coordinates.')
 
-
 def generateComputerPlayerMove():
     pass
 
 
-def minimax(board):
-    b = copy.deepcopy(board)
+# right now minimax returns a state in the highest position every time
+def minimax(board, depth, alpha, beta, maximizingPlayer):
+    curState = copy.deepcopy(board)
+    state = None
+    children = curState.generateStates()
 
+    # for s in children:
+    #     s.draw()
+
+    if (depth == 0 or curState.checkForAWin('M')):
+        return curState, staticEvaluation(curState)
+
+    if maximizingPlayer:
+        maxEvaluation = -1000
+        
+        # curState is the generated States
+        for child in children:
+            state, evaluation = minimax(child, depth -1, alpha, beta, False)
+            maxEvaluation = max(maxEvaluation, evaluation)
+            alpha = max(alpha, evaluation)
+            if beta <= alpha:
+                break
+        # print("maximizing")
+        # state.draw()
+        return state, maxEvaluation
+
+    else:
+        minEvaluation = 1000
+        for child in children:
+            state, evaluation = minimax(child, depth - 1, alpha, beta, True)
+            minEvaluation = min(minEvaluation, evaluation)
+            beta = min(beta, evaluation)
+            if beta <= alpha:
+                break
+        # print("minimizing")
+        # state.draw()
+        return state, minEvaluation
+    
+
+def staticEvaluation(curState): #curState is a node and this assigns a value to it
+    random.seed(datetime.now())
+    curState.heuristic = random.randint(1, 50)
+    # print(f"heuristic value = {curState.heuristic}")
+    return curState.heuristic
 
 def alpha_beta(board):
     b = copy.deepcopy(board)
